a570caa3b73afc84f4a93b392e8cb6fc24b3cd19
Chao An <chaoan@brandeis.edu>
2024-11-19 23:12:13 +0000

    added file from fiducial, revised my_odom

--- src/my_odom.py
+++ src/my_odom.py
@@ -8,11 +8,14 @@ from tf.transformations import euler_from_quaternion
 
 class MyOdom:
     def __init__(self):
+        # Subscribe to both robot odometries
         self.roba_odom_sub = rospy.Subscriber('/roba/odom', Odometry, self.roba_odom_cb)
-        self.roba_my_odom_pub = rospy.Publisher('my_odom', Point, queue_size=1)
         self.robb_odom_sub = rospy.Subscriber('/robb/odom', Odometry, self.robb_odom_cb)
-        self.robb_my_odom_pub = rospy.Publisher('my_odom', Point, queue_size=1)
+        
+        # Single publisher for Point[] message
+        self.my_odom_pub = rospy.Publisher('my_odom', Point[], queue_size=1)
 
+        # Tracking variables for each robot
         self.roba_old_pose = None 
         self.roba_dist = 0.0
         self.roba_yaw = 0.0
@@ -20,108 +23,90 @@ class MyOdom:
         self.robb_old_pose = None 
         self.robb_dist = 0.0
         self.robb_yaw = 0.0
+        
+        # Flag to ensure we have data from both robots before publishing
+        self.roba_updated = False
+        self.robb_updated = False
                 
     def roba_odom_cb(self, msg):
-        """Callback function for `odom_sub`."""
+        """Callback function for roba odometry."""
         cur_pose = msg.pose.pose
-        self.roba_update_dist(cur_pose) 
-        self.roba_update_yaw(cur_pose.orientation)
-        self.roba_publish_data()
-
-    def roba_update_dist(self, cur_pose):
-        """
-        Helper to `odom_cb`.
-        Updates `self.dist` to the distance between `self.old_pose` and
-        `cur_pose`.
-        """
-        if self.roba_old_pose is not None:
-            x_diff = cur_pose.position.x - self.roba_old_pose.position.x
-            y_diff = cur_pose.position.y - self.roba_old_pose.position.y
-            self.roba_dist += math.sqrt(x_diff ** 2 + y_diff ** 2)
-        self.roba_old_pose = cur_pose
-
-    def roba_update_yaw(self, cur_orientation):
-        """
-        Helper to `odom_cb`.
-        Updates `self.yaw` to current heading of robot.
-        """
-        orientations = [cur_orientation.x,
-                cur_orientation.y,
-                cur_orientation.z,
-                cur_orientation.w]
-        (roll, pitch, yaw) = euler_from_quaternion(orientations)
-        # From the robot's point of view, `odom` publishes values in: (i) [0, pi)
-        # for the range [12, 6) o'clock counter-clockwise, but (ii) (0, -pi] for 
-        # the range (12, 6] clockwise. The line below converts values in (ii) to
-        # be in the range [pi, 2 * pi), so that we have values in [0, 2 * pi -1) 
-        # for the entire [12, 12) o'clock counter-clockwise range.
-        self.roba_yaw = yaw if yaw >= 0 else 2 * math.pi + yaw
-
-    def roba_publish_data(self):
-        """
-        Publish `self.dist` and `self.yaw` on the `my_odom` topic.
-        """
-        # The `Point` object we create below is not used as a geometric point,
-        # but simply as a data container for `self.dist` and `self.yaw` so we can
-        # publish it on `my_odom`.
-        data = Point()
-        data.x = self.roba_dist
-        data.y = self.roba_yaw
-        data.z = self.robb_dist
-        self.roba_my_odom_pub.publish(data)
-
+        self.update_dist('roba', cur_pose) 
+        self.update_yaw('roba', cur_pose.orientation)
+        
     def robb_odom_cb(self, msg):
-        """Callback function for `odom_sub`."""
+        """Callback function for robb odometry."""
         cur_pose = msg.pose.pose
-        self.robb_update_dist(cur_pose) 
-        self.robb_update_yaw(cur_pose.orientation)
-        self.robb_publish_data()
+        self.update_dist('robb', cur_pose) 
+        self.update_yaw('robb', cur_pose.orientation)
+        
+    def update_dist(self, robot, cur_pose):
+        """
+        Updates distance for the specified robot.
+        """
+        if robot == 'roba':
+            old_pose = self.roba_old_pose
+            if old_pose is not None:
+                x_diff = cur_pose.position.x - old_pose.position.x
+                y_diff = cur_pose.position.y - old_pose.position.y
+                self.roba_dist += math.sqrt(x_diff ** 2 + y_diff ** 2)
+            self.roba_old_pose = cur_pose
+            self.roba_updated = True
+        
+        elif robot == 'robb':
+            old_pose = self.robb_old_pose
+            if old_pose is not None:
+                x_diff = cur_pose.position.x - old_pose.position.x
+                y_diff = cur_pose.position.y - old_pose.position.y
+                self.robb_dist += math.sqrt(x_diff ** 2 + y_diff ** 2)
+            self.robb_old_pose = cur_pose
+            self.robb_updated = True
+        
+        # Publish if we have updates from both robots
+        if self.roba_updated and self.robb_updated:
+            self.publish_data()
 
-    def robb_update_dist(self, cur_pose):
+    def update_yaw(self, robot, cur_orientation):
         """
-        Helper to `odom_cb`.
-        Updates `self.dist` to the distance between `self.old_pose` and
-        `cur_pose`.
+        Updates yaw for the specified robot.
         """
-        if self.robb_old_pose is not None:
-            x_diff = cur_pose.position.x - self.robb_old_pose.position.x
-            y_diff = cur_pose.position.y - self.robb_old_pose.position.y
-            self.robb_dist += math.sqrt(x_diff ** 2 + y_diff ** 2)
-        self.robb_old_pose = cur_pose
-
-    def robb_update_yaw(self, cur_orientation):
-        """
-        Helper to `odom_cb`.
-        Updates `self.yaw` to current heading of robot.
-        """
-        orientations = [cur_orientation.x,
-                cur_orientation.y,
-                cur_orientation.z,
-                cur_orientation.w]
+        orientations = [
+            cur_orientation.x,
+            cur_orientation.y,
+            cur_orientation.z,
+            cur_orientation.w
+        ]
         (roll, pitch, yaw) = euler_from_quaternion(orientations)
-        # From the robot's point of view, `odom` publishes values in: (i) [0, pi)
-        # for the range [12, 6) o'clock counter-clockwise, but (ii) (0, -pi] for 
-        # the range (12, 6] clockwise. The line below converts values in (ii) to
-        # be in the range [pi, 2 * pi), so that we have values in [0, 2 * pi -1) 
-        # for the entire [12, 12) o'clock counter-clockwise range.
-        self.robb_yaw = yaw if yaw >= 0 else 2 * math.pi + yaw
+        
+        # Convert negative yaw to [0, 2π) range
+        yaw = yaw if yaw >= 0 else 2 * math.pi + yaw
+        
+        if robot == 'roba':
+            self.roba_yaw = yaw
+        elif robot == 'robb':
+            self.robb_yaw = yaw
 
-    def robb_publish_data(self):
+    def publish_data(self):
         """
-        Publish `self.dist` and `self.yaw` on the `my_odom` topic.
+        Publish data for both robots as a Point[] message.
         """
-        # The `Point` object we create below is not used as a geometric point,
-        # but simply as a data container for `self.dist` and `self.yaw` so we can
-        # publish it on `my_odom`.
-        data = Point()
-        data.x = self.roba_dist
-        data.y = self.roba_yaw
-        data.z = self.robb_dist
-        #data.y_b = self.robb_yaw
-        self.robb_my_odom_pub.publish(data)
+        # Create a list of Points
+        data = [
+            Point(x=self.roba_dist, y=self.roba_yaw, z=0),  # roba point
+            Point(x=self.robb_dist, y=self.robb_yaw, z=0)   # robb point
+        ]
+        
+        # Publish the Point[] message
+        self.my_odom_pub.publish(data)
+        
+        # Reset update flags
+        self.roba_updated = False
+        self.robb_updated = False
 
-if __name__ == '__main__':
+def main():
     rospy.init_node('my_odom')
-
     MyOdom()
     rospy.spin()
+
+if __name__ == '__main__':
+    main()
